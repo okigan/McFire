9e2fcbacd903d5c4b55bd028384ef860ae304610
Igor Okulist <iokulist@netflix.com>
2019-06-18 01:02:17 -0500

    more tests

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 jupytext --test --to ipynb mcfire/mcfire.py
 jupytext --to ipynb mcfire/mcfire.py
+jupytext --test --to py:percent mcfire/mcfire.ipynb
 pycodestyle -v --exclude=*.pynb_checkpoints/* mcfire
 py.test -v
 
